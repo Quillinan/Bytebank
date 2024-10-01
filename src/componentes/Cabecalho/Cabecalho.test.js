b78311c17dfb612c8c654b9f2aac996fb1e80642
Quillinan <milton.quillinan@gmail.com>
2024-10-01 01:38:26 -0300

    test: add snapchat test

--- src/componentes/Cabecalho/Cabecalho.test.js
+++ src/componentes/Cabecalho/Cabecalho.test.js
@@ -5,4 +5,5 @@ test("Deve renderizar o nome do usuário logado", () => {
   render(<Cabecalho />);
   const nomeUsuario = screen.getByText("Joana Fonseca Gomes");
   expect(nomeUsuario).toBeInTheDocument();
+  expect(nomeUsuario).toMatchSnapshot();
 });
