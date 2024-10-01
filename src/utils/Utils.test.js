295c71790744a7a319c44b4b943588a66f4c8a42
Quillinan <milton.quillinan1798@gmail.com>
2024-10-01 11:36:26 -0300

    test: add calculaNovoSaldo mock

--- src/utils/Utils.test.js
+++ src/utils/Utils.test.js
@@ -1,11 +1,36 @@
+import { calculaNovoSaldo } from ".";
+
 describe("Deve retornar o valor do saldo", () => {
+  const saldo = 100;
+
+  const listaDeTransações = [
+    {
+      transacao: "Depósito",
+      valor: 50,
+    },
+    {
+      transacao: "Transferência",
+      valor: 50,
+    },
+  ];
   test("Atualizado com o rendimento", () => {
     const calculaRendimento = jest.fn((saldo) => saldo + saldo * 0.005);
-    const saldo = 100;
     const novoSaldo = calculaRendimento(saldo);
 
     expect(novoSaldo).toBe(100.5);
     expect(calculaRendimento).toBeCalled();
     expect(calculaRendimento).toBeCalledWith(saldo);
+  });
+  test("Aumentar no depósito", () => {
+    const calculaSaldo = jest.fn(calculaNovoSaldo);
+
+    const novoSaldo = calculaSaldo(listaDeTransações[0], saldo);
+    expect(novoSaldo).toBe(150);
+  });
+  test("Diminuir na transferência", () => {
+    const calculaSaldo = jest.fn(calculaNovoSaldo);
+
+    const novoSaldo = calculaSaldo(listaDeTransações[1], saldo);
+    expect(novoSaldo).toBe(50);
   });
 });
