ef5c3aafea349c01ef3ca5aa62be9324459197fd
Quillinan <milton.quillinan1798@gmail.com>
2024-10-01 10:22:15 -0300

    test: transacao add rerender

--- src/componentes/Extrato/Transacoes/Transacoes.test.js
+++ src/componentes/Extrato/Transacoes/Transacoes.test.js
@@ -7,12 +7,30 @@ describe("Deve renderizar o mesmo componente", () => {
     transacao: "Depósito",
     valor: 100,
   };
+
+  const novaTransacao = {
+    transacao: "Transferência",
+    valor: 50,
+  };
+
   test("Com props atualizadas", () => {
     render(<Transacoes transacao={transacao} estilos={estilos} />);
     const tipoTransacao = screen.getByTestId("tipoTransacao");
     const valorTransacao = screen.getByTestId("valorTransacao");
 
     expect(tipoTransacao).toHaveTextContent("Depósito");
-    expect(valorTransacao).toHaveTextContent("100");
+    expect(valorTransacao).toHaveTextContent("R$ 100");
+  });
+  test("Com props atualizadas via rerender", () => {
+    const { rerender } = render(
+      <Transacoes transacao={transacao} estilos={estilos} />
+    );
+
+    rerender(<Transacoes transacao={novaTransacao} estilos={estilos} />);
+    const novoTipoTransacao = screen.getByTestId("tipoTransacao");
+    const novoValorTransacao = screen.getByTestId("valorTransacao");
+
+    expect(novoTipoTransacao).toHaveTextContent("Transferência");
+    expect(novoValorTransacao).toHaveTextContent("- R$ 50");
   });
 });
